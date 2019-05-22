acb13c70cfdf21b058a96d77eec29a6f6599d6ae
ulucay <31716850+ulucay@users.noreply.github.com>
2019-05-21 23:03:03 -0500

    brainstorming

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,31 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game{
+    constructor(){
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    startGame(){
+
+    }
+
+    getRandomPhrase(){
+
+    }
+
+    handleInteraction(){
+
+    }
+
+    removeLife(){
+
+    }
+
+    checkForWin(){
+
+    }
+
+    gameOver(){
+
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,2 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+const phrase = new Phrase('Life is like a box of chocolates');
+const game = new Game();
